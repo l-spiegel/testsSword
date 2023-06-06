38ebfcce476d72935283ae7e576154f4645a5f6a
FilipaSS <113024209+FilipaSS@users.noreply.github.com>
2023-06-06 11:39:05 +0100

    regression-test-creation regression-test-final-code-review regression-test-alterations regression-test-ptmsat-creation regression-test-ptmsat-alterations

--- src/main/java/appium/Android/swordRegressionPtMsat.java
+++ src/main/java/appium/Android/swordRegressionPtMsat.java
@@ -34,13 +34,12 @@ public class swordRegressionPtMsat {
         utilitiesAndroid.login("l.spiegel+3@swordhealth.com", "Test1234!", driver);
 
         try {
-            Thread.sleep(3000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
 
         //validar ecrã PTMSAT
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Share your thoughts here']")));
         driver.findElementByXPath("//android.widget.TextView[@text='How would you rate your experience with Luiza Almeida?']");
         driver.findElementByXPath("//android.widget.TextView[@text='How could we improve your experience?']");
         driver.findElementByXPath("//android.widget.TextView[@text='Share your thoughts here']");
@@ -51,10 +50,13 @@ public class swordRegressionPtMsat {
         //Tap on the fourth star → Tap "Share feedback" button → Error message
         //Select some options (other include) → Tap "Share feedback" button → Error message
         utilitiesAndroid.clickByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]/android.view.View[1]/android.widget.Button", driver);
+        driver.findElementByXPath("//android.widget.TextView[@text='Please rate your experience']");
         utilitiesAndroid.clickByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]/android.widget.ScrollView/android.view.View[4]/android.view.View", driver);
-        utilitiesAndroid.clickByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]/android.widget.ScrollView/android.view.View[4]", driver);
         utilitiesAndroid.clickByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]/android.view.View[1]/android.widget.Button", driver);
+        driver.findElementByXPath("//android.widget.TextView[@text='Please select at least one option']");
         utilitiesAndroid.clickByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]/android.widget.ScrollView/android.widget.TextView[6]", driver);
+        utilitiesAndroid.clickByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]/android.view.View[1]/android.widget.Button", driver);
+        driver.findElementByXPath("//android.widget.TextView[@text='Please share some feedback']");
 
         //Write something on the text box
         //Unselected the option "Other"
@@ -76,13 +78,20 @@ public class swordRegressionPtMsat {
         MobileElement ptCard = driver.findElementByAccessibilityId("home_card_pt");
         MobileElement weeklyCard = driver.findElementByAccessibilityId("home_card_weekly_goal");
         mobileActions.swipeByElements(ptCard, weeklyCard);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Share your thoughts here']")));
 
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        //Tap 2x same star - Tap fifth star → tap on the same star - nothing should happen
         //Tap one star (fifth one) → unselect one star → Tap on "Other" option → Select one more star
         //Unselect one star → unselect another star → unselect another star → unselect another star
         //Select one more star → Select one more star → Select one more star → Select one more star
         //Tap "Share feedback" button → Tap "Close" button
 
+        utilitiesAndroid.clickByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]/android.widget.ScrollView/android.view.View[5]", driver);
         utilitiesAndroid.clickByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]/android.widget.ScrollView/android.view.View[5]", driver);
         utilitiesAndroid.clickByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]/android.widget.ScrollView/android.view.View[4]", driver);
         utilitiesAndroid.clickByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]/android.widget.ScrollView/android.widget.TextView[6]", driver);
