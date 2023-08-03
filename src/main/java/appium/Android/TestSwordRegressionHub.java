b484cb76365095d5e4bbdac104920b9e4df9adde
Luiza Spiegel <l.spiegel@swordhealth.com>
2023-08-03 19:42:56 +0100

    new tests and baselines for hub screen

--- src/main/java/appium/Android/TestSwordRegressionHub.java
+++ src/main/java/appium/Android/TestSwordRegressionHub.java
@@ -22,6 +22,8 @@ public class TestSwordRegressionHub {
 	private final static String CHECK_HUB_ENROLLED_TOP_SCREEN = "hub_enrolled_top";
 	private final static String CHECK_HUB_ENROLLED_BLOOM_POPUP = "hub_enrolled_bloom_popup";
 	private final static String CHECK_HUB_ENROLLED_MOVE_POPUP = "hub_enrolled_move_popup";
+	private final static String CHECK_HUB_NOT_ELIGIBLE_TOP_SCREEN = "hub_not_eligible_top";
+	private final static String CHECK_HUB_INTERESTED_POPUP = "hub_interested_popup";
 
 	private AndroidDriver<MobileElement> driver;
 	@Before
@@ -190,6 +192,7 @@ public class TestSwordRegressionHub {
 		new VisualCheck(driver);
 
 		//login
+		System.out.println("Ligou o proxy??");
 		utilitiesAndroid.newLogin("luiza.preventive@sword.com", "Test1234!", driver);
 		try {
 			Thread.sleep(2000);
@@ -235,6 +238,58 @@ public class TestSwordRegressionHub {
 		} catch (InterruptedException e) {
 			Thread.currentThread().interrupt();
 		}
+
+		ConfigurationsAndroid.killDriver();
+	}
+
+	@Test
+	public void hubScreenUnderageDptNotEligible() throws Exception {
+		MobileActions mobileActions = new MobileActions(driver);
+		UtilitiesAndroid utilitiesAndroid = new UtilitiesAndroid();
+		new VisualCheck(driver);
+
+		//login
+		System.out.println("Ligou o proxy??");
+		utilitiesAndroid.newLogin("underage@sword.com", "Test1234!", driver);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+		//validate not eligible card
+		driver.findElementByXPath("//android.widget.TextView[@text=\"Not eligible\"]");
+		driver.findElementByXPath("//android.widget.TextView[@text=\"I’m interested\"]");
+		VisualCheck.doVisualCheck(CHECK_HUB_NOT_ELIGIBLE_TOP_SCREEN);
+		//validate not eligible popup
+		utilitiesAndroid.clickByAccessibilityId("on_call_programs_card_0_interested_button", driver);
+		driver.findElementByXPath("//android.widget.TextView[@text=\"Thank you for your interest. We'll notify your benefits manager.\"]");
+		VisualCheck.doVisualCheck(CHECK_HUB_INTERESTED_POPUP);
+		utilitiesAndroid.clickByXPath("//android.widget.Button", driver);
+		//scroll to show more cards
+		MobileElement programsCard0 = driver.findElementByAccessibilityId("on_call_programs_card_0");
+		MobileElement homeOnCallCard = driver.findElementByAccessibilityId("home_on_call_card");
+		mobileActions.swipeByElements(programsCard0, homeOnCallCard);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+		MobileElement programsCard2 = driver.findElementByAccessibilityId("on_call_programs_card_2");
+		mobileActions.swipeByElements(programsCard2, programsCard0);
+		//tap bloom get started button
+		utilitiesAndroid.clickByAccessibilityId("on_call_programs_card_1_get_started_button", driver);
+		//validate bloom underage bottom sheet
+		driver.findElementByXPath("//android.widget.TextView[@text=\"Thanks for your interest in Bloom!\"]");
+		driver.findElementByXPath("//android.widget.TextView[@text=\"Unfortunately, we require Bloom members to be at least 18 years old. We hope to see you enroll in the future!\"]");
+		driver.findElementByXPath("//android.widget.TextView[@text=\"Got it\"]");
+		utilitiesAndroid.clickByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[2]/android.view.View/android.widget.Button", driver);
+		//tap move get started button
+		utilitiesAndroid.clickByAccessibilityId("on_call_programs_card_2_get_started_button", driver);
+		//validate move underage bottom sheet
+		driver.findElementByXPath("//android.widget.TextView[@text=\"Thanks for your interest in Move!\"]");
+		driver.findElementByXPath("//android.widget.TextView[@text=\"Unfortunately, we require Move members to be at least 18 years old. We hope to see you enroll in the future!\"]");
+		driver.findElementByXPath("//android.widget.TextView[@text=\"Got it\"]");
+		mobileActions.tapByCoordinates(785, 1181);
 
 		ConfigurationsAndroid.killDriver();
 	}
