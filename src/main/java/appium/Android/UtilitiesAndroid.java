9be3a38ad1c5abaa13c4138bf37f46beaed25d9c
FilipaSS <113024209+FilipaSS@users.noreply.github.com>
2023-09-12 11:59:57 +0100

    updates_xpaths_release520 updates_xpaths_release600 updates_xpaths_release610 updates_devices_Filipa updates_strings_times_buildFinalSprint_device_Filipa updates_strings_buildFinalSprint_deviceAndroid_Filipa update_notes_iOS_Filipa update_users_release620_iOS update_times_Android new_test_settings

--- src/main/java/appium/Android/UtilitiesAndroid.java
+++ src/main/java/appium/Android/UtilitiesAndroid.java
@@ -30,4 +30,24 @@ public class UtilitiesAndroid {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Create your PIN code']")));
         driver.findElementByXPath("//android.widget.TextView[@text='Not now']").click();
     }
+    public void newLogin(String email, String password, AndroidDriver<MobileElement> driver) {
+        WebDriverWait wait = new WebDriverWait(driver,20);
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Your journey to a pain-free life starts here']")));
+        MobileElement emailField = driver.findElementByXPath("//android.widget.EditText");
+        emailField.clear();
+        emailField.sendKeys(email);
+        driver.findElementByAccessibilityId("continueButton").click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Password']")));
+        MobileElement passField = driver.findElementByXPath("//android.widget.EditText");
+        passField.sendKeys(password);
+        driver.findElementByAccessibilityId("loginButton").click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Create PIN code']")));
+        driver.findElementByXPath("//android.widget.TextView[@text='Not now']").click();
+    }
 }
