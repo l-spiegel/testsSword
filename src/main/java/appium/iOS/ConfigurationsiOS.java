9a5f2c6fefaaf8039669ee5a003adab255744305
Luiza Spiegel <l.spiegel@swordhealth.com>
2023-11-06 13:07:03 +0000

    new baselines because of changes in the screenshots sizes new capabilities to run appium in iOS 17

--- src/main/java/appium/iOS/ConfigurationsiOS.java
+++ src/main/java/appium/iOS/ConfigurationsiOS.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ConfigurationsiOS {
 
-    public final static String VALIDATION_PATH = "src/visual tests/iPhone 11 Pro/PT MSat"; //GitHub path to baselines. It should be something like "src/visual tests/DEVICE/TEST" and must be changed for every test performed
+    public final static String VALIDATION_PATH = "src/visual tests/iPhone 11 Pro/Login"; //GitHub path to baselines. It should be something like "src/visual tests/DEVICE/TEST" and must be changed for every test performed
     private static IOSDriver<MobileElement> driver;
 
     public static IOSDriver<MobileElement> getDriver() throws MalformedURLException {
@@ -22,17 +22,17 @@ public class ConfigurationsiOS {
     private static void createDriver() {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "iOS");
-        desiredCapabilities.setCapability("platformVersion", "16.4.1"); //your device iOS version
+        desiredCapabilities.setCapability("platformVersion", "17.0.3"); //your device iOS version
         desiredCapabilities.setCapability("deviceName", "iPhone 11 Pro"); //your device model
         desiredCapabilities.setCapability("udid", "00008030-001115363A7A802E"); //your device UDID
         desiredCapabilities.setCapability("automationName", "XCUITest");
         desiredCapabilities.setCapability("xcodeOrgId", "698N4JU9B9");
         desiredCapabilities.setCapability("xcodeSigningId", "iPhone Developer");
-        desiredCapabilities.setCapability("app", "/Users/luizaspiegel/Downloads/SWORDHealth6004.ipa"); //local path to the test build
+        desiredCapabilities.setCapability("app", "/Users/luizaspiegel/Downloads/SWORDHealth640.ipa"); //local path to the test build
         desiredCapabilities.setCapability("updatedWDABundleId", "com.luizateste2.wda.runner"); //your WDA bundle id
         desiredCapabilities.setCapability("showXcodeLog", "true");
         desiredCapabilities.setCapability("wdaLocalPort", "8205");
-        desiredCapabilities.setCapability("usePrebuiltWDA", "true");
+        desiredCapabilities.setCapability("useNewWDA", "true");
 
         try {
             driver = new IOSDriver<>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
